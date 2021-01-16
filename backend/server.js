95a7c1611cc68f8eb833127d7058312c81d6e11e
RJDulac <analyticrye@gmail.com>
2021-01-16 17:22:39 -0500

    Redux implemented for products and product details

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import colors from "colors";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -12,6 +13,10 @@ connectDB();
 const app = express();
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
