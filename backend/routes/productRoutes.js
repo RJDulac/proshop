dfe27475e880d3e4fa9ecadfc6c99e136411d427
RJDulac <analyticrye@gmail.com>
2021-01-27 14:14:10 -0500

    controllers added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,13 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 
-import Product from "../model/productModel.js";
-
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 const router = express.Router();
 
-//@dec Fetch all products
-//@route Get /api/products
-//@access Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
 
-//@dec Fetch a single products
-//@route Get /api/products/:id
-//@access Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
