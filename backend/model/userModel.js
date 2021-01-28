09e3ff82e047b40807174599b98c924aa41db019
RJDulac <analyticrye@gmail.com>
2021-01-27 19:22:21 -0500

    User registration and password encryption added

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -30,6 +30,17 @@ const userSchema = mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+//run bcrypt before saving
+userSchema.pre("save", async function (next) {
+  //check if password has not been modified
+  if (!this.isModified("password")) {
+    next();
+  }
+  //encrpyt on save
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 const User = mongoose.model("User", userSchema);
 
 export default User;
