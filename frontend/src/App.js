ff146615bfa23d710f3237c90471ea8dafa4b0ad
RJDulac <analyticrye@gmail.com>
2021-01-29 19:18:46 -0500

    fixed bugs in form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
-import ProfileScreen from "./screens/ProductScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const App = () => {
   return (
